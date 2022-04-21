fd63265c0c1c5acd34c087dfa622ae74d8726529
Jak Major <66277064+jakmajor@users.noreply.github.com>
2022-04-21 17:22:51 -0500

    working

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -2,21 +2,30 @@ def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
+
+  favorite_hash = {
+    :pink => "pink", 
+    :three => "3"
+  }
+
 end
 
 def favorite_color(favorite_list)
   # return the value of the color key
+  favorite_hash[:pink]
 end
 
 def favorite_number(favorite_list)
   # use #fetch to return the value of the number key or 42 if the key is not found
+  favorite_hash.fetch("3", "42")
 end
 
 def update_favorite_movie(favorite_list, movie)
   # Step 1: add/update the key of movie (as a symbol)
 
+  favorite_hash["movie"] = "what about bob?"
   # Step 2: return the hash (because Step 1 returns the value of the movie key)
-  favorite_list
+  return favorite_list 
 end
 
 def remove_favorite_number(favorite_list)
